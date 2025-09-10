28f0a07e21f824a188e5a12644c54bf31a3bb93d
Annus <dev.muhammad.annus@gmail.com>
2025-09-11 02:04:28 +0500

    feat: add main file (entry_point) to start and test the project and file to keep the central point that require all the modules

---
+++ lib/acme_basket.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Central require to load the library in one go
+
+require_relative 'money'
+require_relative 'product'
+require_relative 'catalogue'
+require_relative 'basket'
+require_relative 'delivery_rule'
+require_relative 'offers/offer'
+require_relative 'offers/red_widget_offer'
